fd55c4a31d26730680c5188626b06085898f482c
phanuwat3487 <boyasd12345@gmail.com>
2024-10-24 22:44:02 +0700

    setup gitignore with pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,12 +15,9 @@ module.exports = {
       user: "root",
       host: "154.197.124.66:2224",
       ref: "origin/main",
-      repo: "git@github.com:generative-promtpay/backend-service.git",
-      path: "/var/gitlab/backend-service",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
+      repo: "git@github.com:PH3487/promtpay-api.git",
+      path: "/var/github/promtpay-api",
+      "post-deploy": "npm install && npm run build && pm2 reload ecosystem.config.js --env production"
     },
   },
 };
