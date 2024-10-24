6b76c26a3ae51a576686701bd7a47916d47dc3ba
phanuwat3487 <boyasd12345@gmail.com>
2024-10-24 23:17:52 +0700

    set wf pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,22 @@
 module.exports = {
   apps: [
     {
-      script: "dist/app.js",
-      watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
+      name: "backend-service",
+      script: "./server.js",
     },
   ],
-
   deploy: {
     production: {
-      user: "root",
-      host: "154.197.124.66:2224",
-      ref: "origin/main",
-      repo: "git@github.com:PH3487/promtpay-api.git",
-      path: "/var/github/promtpay-api",
-      "post-deploy": "npm install && npm run build && pm2 reload ecosystem.config.js --env production"
+      user: "root", // Replace with your SSH username
+      host: "154.197.124.66", // Your server's IP
+      ref: "origin/main", // Branch to deploy from
+      repo: "git@github.com:PH3487/promtpay-api.git", // Your repository
+      path: "/var/www/backend-service", // Path on the server where the app will be deployed
+      "pre-deploy-local": "", // Commands to run locally before deployment
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production", // Commands to run after deployment
+      ssh_options: ["StrictHostKeyChecking=no"], // Optional, for bypassing SSH key checks
+      port: "2224", // Specify the port here
     },
   },
 };
